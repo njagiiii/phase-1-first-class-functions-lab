f619761ce9d0e349c047aaca0ef241802f5d0f77
njagiiii <grace.makena@student.moringaschool.com>
2023-07-06 07:28:52 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(arr){
+    arr =['Antonia','Nuru','Amari','Mo'];
+    return arr.slice(0,arr.length-2);
+}
+const returnLastTwoDrivers=function(arr){
+    arr =['Antonia','Nuru','Amari','Mo'];
+    return arr.slice(2);
+}
+const selectingDrivers =[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function FareMultiplier(){return 5*x};
+}
+function fareDoubler(x){
+    return 2*x;
+}
+function fareTripler(x){
+    return 3*x;
+}
+function selectDifferentDrivers(arrayofDrivers,returnFirstTwoDrivers){
+    arrayofDrivers['Antonia','Nuru','Amari','Mo']
+    returnFirstTwoDrivers();
+}
